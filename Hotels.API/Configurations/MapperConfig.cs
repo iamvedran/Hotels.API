0be6c16fb4b7da147a0d05de884d3d533431f935
vedran667 <vedran666@yahoo.com>
2023-01-08 02:09:15 +0100

    added DTO and automapper

---
+++ Hotels.API/Configurations/MapperConfig.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Hotels.API.Data;
+using Hotels.API.Models.Hotel;
+
+namespace Hotels.API.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        public MapperConfig()
+        {
+            CreateMap<Hotel, HotelDTO>().ReverseMap();
+        }
+    }
+}
---
+++ Hotels.API/Models/Hotel/HotelDTO.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Build.Framework;
+
+namespace Hotels.API.Models.Hotel
+{
+    public class HotelDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public double Longitude { get; set; }
+        [Required]
+        public double Latutude { get; set; }
+    }
+}
